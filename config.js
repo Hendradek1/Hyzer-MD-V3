ad776746aafc36df09cf003156b7af144604a4d6
Hendradek1 <106667777+Hendradek1@users.noreply.github.com>
2022-06-04 00:43:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,83 +1,83 @@
 //=============『 Utama 』================== //
-global.owner = ['4365070002069'] 
+global.owner = ['6287892711054'] 
 global.mods = [] 
 global.prems = [] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'Hendraaaa.'
-global.numberowner = '4365070002069'
-global.instagram = ''
-global.github = ''
-global.dana = '083181085070'
-global.pulsa = '083181085070'
-global.gopay = '083181085070'
+global.nameowner = 'Hyzer Official'
+global.numberowner = '6287892711054'
+global.instagram = 'https://instagram.com/hyzer_stfu'
+global.github = 'https://github.com/Hyzerr'
+global.dana = '087892711054'
+global.pulsa = '085212023766'
+global.gopay = '087892711054'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'Hendra-Md'
-global.gc = ''
-global.web = '' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.namebot = 'ShiroBot-Md'
+global.gc = 'https://chat.whatsapp.com/Jzd9DEVB5nODtNBk1VCNrV'
+global.web = 'https://hyzerr-apii.herokuapp.com' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = '' //ganti jadi foto bot mu
-global.fotonya2 = '' //ini juga ganti 
+global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = 'hendra-𝙼𝙳'
+global.wm = '𝚂𝙷𝙸𝚁𝙾-𝙼𝙳'
 global.watermark = wm
 global.wm2 = '                     「 𝚂𝙷𝙸𝚁𝙾-𝙼𝙳 あ⁩ 」'
-global.wm3 = '⫹⫺ hendra-𝙼𝙳'
+global.wm3 = '⫹⫺ 𝚂𝙷𝙸𝚁𝙾-𝙼𝙳'
 global.htki = '––––––『' 
 global.htka = '』––––––'
-global.media = ''
-global.fla = '&text='
+global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.wait = '_*tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = '2022'
-global.author = '© Hendra dek'
+global.author = '© Hyzer Official'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
-  // name: ''
-  amel: '',
-    bx: '',
-  dhnjing: '',
-  hardianto: '',
-  jonaz: '',
-  neoxr: '',
-  nrtm: '',
-  xteam: '',
-  nzcha: '',
-  bg: '',
-  fdci: '',
-  dzx: '',
-  bsbt: '',
-  zahir: '',
-  zeks: '',
-  zekais: '',
-  hardianto: '',
-  pencarikode: '', 
-  erdwepe: '',
-  lolhuman: '',
-  LeysCoder: '',
-  rey: ''
+  // name: 'https://website'
+  amel: 'https://melcanz.com',
+    bx: 'https://bx-hunter.herokuapp.com',
+  dhnjing: 'https://dhnjing.xyz',
+  hardianto: 'https://hardianto-chan.herokuapp.com',
+  jonaz: 'https://jonaz-api-v2.herokuapp.com',
+  neoxr: 'https://neoxr-api.herokuapp.com',
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  nzcha: 'http://nzcha-apii.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  fdci: 'https://api.fdci.se',
+  dzx: 'https://api.dhamzxploit.my.id',
+  bsbt: 'https://bsbt-api-rest.herokuapp.com',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  zekais: 'http://zekais-api.herokuapp.com',
+  hardianto: 'https://hardianto-chan.herokuapp.com',
+  pencarikode: 'https://pencarikode.xyz', 
+  erdwepe: 'https://erdwpe-api.herokuapp.com',
+  lolhuman: 'https://api.lolhuman.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  rey: 'https://server-api-rey.herokuapp.com'
 }
 global.APIKeys = { // APIKey Here
-  // '': 'apikey'
-  '': 'elaina',
-  '': 'apirey',
-  '': 'd37372311698ed1d',
-  '': 'zahirgans',
-  '': 'benniismael',
-  '': 'apivinz',
-  '': 'hardianto',
-  '': 'pais', 
-  '': 'dappakntlll',
-  '': 'apikeymu',
-  '': 'Hendrabotz',
+  // 'https://website': 'apikey'
+  'https://melcanz.com': 'elaina',
+  'https://server-api-rey.herokuapp.com': 'apirey',
+  'https://api.xteam.xyz': 'd37372311698ed1d',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://bsbt-api-rest.herokuapp.com': 'benniismael',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://hardianto-chan.herokuapp.com': 'hardianto',
+  'https://pencarikode.xyz': 'pais', 
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll',
+  'https://zekais-api.herokuapp.com': 'apikeymu',
+  'https://api.lolhuman.xyz': 'Deffbotz',
 }
 
 //=============『 RPG GAMES 』================== //
